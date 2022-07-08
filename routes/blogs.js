34c1353e898d3a64daadc3001cf2c334b7f17f7e
Goncalo <goncalosantos@Goncalos-MacBook-Pro.local>
2022-07-08 11:17:02 -0400

    hw

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,8 +1,25 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { blogsDB } = require("./mongo");
 
-router.get("/hello-blogs", (req, res) => {
-  res.json({ message: "hello from express" });
+router.get("/hello-blogs", (req, res, next) => {
+  res.json({ message: "Hello from express" });
+});
+
+router.get("/all-blogs", async (req, res, next) => {
+  const limit = Number(req.query.limit);
+  const sortField = req.query.sortField;
+  const sortOrder = req.query.sortOrder;
+  const filterField = req.query.filterField;
+  const filterValue = req.query.filterValue;
+  
+  try {
+    const collection = await blogsDB().collection("posts");
+    const allBlogs = await collection.find({}).toArray();
+    res.json(allBlogs);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
